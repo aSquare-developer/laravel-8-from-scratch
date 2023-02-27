07e685e8e0a09003d656553c68bfa1c9275dd7f1
aSquare <artur.anissimov@gmail.com>
2023-02-27 17:35:00 +0200

    Store Blog Posts as HTML Files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,13 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>My Blog</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <?= $post; ?>
     </article>
 
     <a href="/">Go back</a>

8c90b25887ef77faa8f0e09e9be4d5b0a55da518
aSquare <artur.anissimov@gmail.com>
2023-02-28 17:41:44 +0200

    Blade: The Absolute Basics

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,11 @@
 </head>
 <body>
     <article>
-        <h1><?= $post->title; ?></h1>
+        <h1>{{ $post->title }}</h1>
 
-        <div><?= $post->body; ?></div>
+        <div>
+            {!! $post->body !!}
+        </div>
     </article>
 
     <a href="/">Go back</a>

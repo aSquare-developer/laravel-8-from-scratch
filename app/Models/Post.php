c50b0d5e4716bdceac2a74a91f3e75375474c86c
aSquare <artur.anissimov@gmail.com>
2023-02-28 16:43:42 +0200

    Collection Sorting and Caching Refresher

---
+++ app/Models/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post extends Model {
+
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function allPosts() {
+
+        return cache()->rememberForever('posts.allPosts', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug,
+                ))->sortByDesc('date');
+        });
+
+    }
+
+    public static function find($slug) {
+        // Of all the blog posts, find the one with a slug that matches the one that was requested.
+        return static::allPosts()->firstWhere('slug', $slug);
+    }
+}

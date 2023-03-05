37b6de84a1b9e150d46b551e4abf0e17b1ce8683
aSquare <artur.anissimov@gmail.com>
2023-03-05 21:31:48 +0200

    Build the Log In Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+
+    public function store() {
+        // Validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Attempt to authenticate and log in the user
+        // based on the provided credentials
+        if(auth()->attempt($attributes)) {
+            // Redirect with success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // Auth failed.
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+    }
+
+    public function create() {
+        return view('sessions.create');
+    }
+
     public function destroy() {
         auth()->logout();
         return redirect('/')->with('success', 'Goodbye!');

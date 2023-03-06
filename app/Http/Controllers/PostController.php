3fb139825c72ea8bfd6218b5f8beb23b1fbae282
aSquare <artur.anissimov@gmail.com>
2023-03-06 23:41:16 +0200

    Activate the Comment Form Route URI Conventions Resourceful Actions Disable Mass Assignment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,5 @@ class PostController extends Controller
         ]);
     }
 
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,12 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'username',
-        'email',
-        'password',
-    ];
+//    protected $fillable = [
+//        'name',
+//        'username',
+//        'email',
+//        'password',
+//    ];
 
     /**
      * The attributes that should be hidden for serialization.

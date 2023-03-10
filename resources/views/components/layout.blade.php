8befb2c4cf50e2e20c9b142f36117db3c515a695
aSquare <artur.anissimov@gmail.com>
2023-03-10 16:03:48 +0200

    Extract a Newsletter Service Extract a Class Automatic Resolution Invokable Controllers

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -52,18 +52,16 @@
 
                 <form method="POST" action="/newsletter" class="lg:flex text-sm">
                     @csrf
+
                     <div class="lg:py-3 lg:px-5 flex items-center">
                         <label for="email" class="hidden lg:inline-block">
                             <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                         </label>
 
-                        <div>
+
                             <input type="email" id="email" name="email" placeholder="Your email address"
                                    class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
-                            @error('email')
-                                <span class="text-xs text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
+
                     </div>
 
                     <button type="submit"
@@ -74,6 +72,9 @@
                 </form>
             </div>
         </div>
+        @error('email')
+        <span class="text-xs text-red-500">{{ $message }}</span>
+        @enderror
     </footer>
 </section>
 

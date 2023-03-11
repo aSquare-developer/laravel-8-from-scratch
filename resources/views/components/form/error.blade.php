d6a864419a21fc2b051d25b50550a6f891aba874
aSquare <artur.anissimov@gmail.com>
2023-03-11 21:30:43 +0200

    Extract Form-Specific Blade Components Form HTML Refactoring Form Blade Components

---
+++ resources/views/components/form/error.blade.php
@@ -0,0 +1,4 @@
+@props(['name'])
+@error('body')
+    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+@enderror
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,59 +9,25 @@
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
-                <div class="mb-6">
-                    <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
-                    <input type="text" name="title" id="title" value="{{ old('title') }}" class="border border-gray-400 p-2 w-full" required>
-                    @error('title')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name="title"/>
+                <x-form.input name="slug"/>
+                <x-form.input name="thumbnail" type="file"/>
+                <x-form.textarea name="excerpt"/>
+                <x-form.textarea name="body"/>
 
-                <div class="mb-6">
-                    <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">Slug</label>
-                    <input type="text" name="slug" id="slug" value="{{ old('slug') }}" class="border border-gray-400 p-2 w-full" required>
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.field>
+                    <x-form.label name="category" />
 
-                <div class="mb-6">
-                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">Thumbnail</label>
-                    <input type="file" name="thumbnail" id="thumbnail" value="{{ old('thumbnail') }}" class="border border-gray-400 p-2 w-full" required>
-                    @error('thumbnail')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">Excerpt</label>
-                    <textarea name="excerpt" id="excerpt" class="border border-gray-400 p-2 w-full" rows="3">{{ old('excerpt') }}</textarea>
-                    @error('excerpt')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
-                    <textarea name="body" id="body" class="border border-gray-400 p-2 w-full" rows="3">{{ old('body') }}</textarea>
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">Category</label>
                     <select name="category_id" id="category_id">
                         @foreach(\App\Models\Category::all() as $category)
                             <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>{{ ucwords($category->name) }}</option>
                         @endforeach
                     </select>
-                    @error('category')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
-                <x-submit-button>Publish</x-submit-button>
+                    <x-form.error name="category" />
+                </x-form.field>
+
+                <x-form.button>Publish</x-form.button>
 
             </form>
         </x-panel>

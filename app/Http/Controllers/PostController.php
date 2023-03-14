5ac6b56d61421539974b324b2814164d090fa0fc
aSquare <artur.anissimov@gmail.com>
2023-03-14 02:01:36 +0200

    All About Authorization Gates Blade Authorization Directives Route Groups Route Resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
     public function index() {
-
+//        $this->authorize('admin');
+//        dd(Gate::allows('admin'));
+//        dd(request()->user()->can('admin'));
         return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString()
         ]);
